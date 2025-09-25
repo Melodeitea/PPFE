2ca26487afeff9295fca2094c51a80df6c13485e
Melodeitea <perlinlily@gmail.com>
2025-09-25 16:01:36 +0200

    wtf

---
+++ Assets/Scripts/PlayerPlaceholder.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlaceholder : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Planet;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Smooth
+
+        //Position
+        transform.position = Vector3.Lerp(transform.position, Player.transform.position, 0.1f);
+
+        Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
+
+        //Rotation
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, gravDirection) * transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 0.1f);
+    }
+
+    public void NewPlanet(GameObject newPlanet)
+    {
+        Planet = newPlanet;
+    }
+}
